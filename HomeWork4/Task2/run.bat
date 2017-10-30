cbd0618370088acaa00fcc633df5f340d275f79b
Алексей <opekun1997@gmail.com>
2017-10-30 21:11:45 +0200

    Final comit

---
+++ HomeWork4/Task2/run.bat
@@ -0,0 +1,2 @@
+java -cp %cd%\program.jar;%cd\lib%cd%\textFileWorker.jar by.grsy.ftf.lesson4task1.Runner
+@pause
---
+++ HomeWork4/Task3/run.bat
@@ -0,0 +1,2 @@
+java -cp %cd%\program.jar;%cd\lib%cd%\textFileWorker.jar by.grsy.ftf.lesson4task1.Runner guestFile.txt masters.txt roomFile.txt serviceFile.txt 
+@pause
